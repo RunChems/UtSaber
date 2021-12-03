54cdd7fb9d2d400dd1285d8003a0fb19ff71a246
caprun <26richardr@gmail.com>
2021-12-02 20:44:02 -0600

    Navbar and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,24 @@
+<div class="container-fluid  shadow-top bg-blue-primary w-100 position-fixed bottom-0">
+    <div class="row">
+        <div class="col-12">
+            <ul class="text-white d-block d-md-flex justify-content-evenly text-center align-items-center h-100">
+                <li>
+                    <a class="text-white" href="">Contacto</a>
+
+                </li>
+                <li>
+                    <a class="text-white" href="">Institución</a>
+
+                </li>
+                <li>
+                    <a class="text-white" href="">Aviso de privacidad</a>
+
+                </li>
+                <li>
+                    <a class="text-white" href="">Powered by <strong>RunChems</strong> </a>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header pt-3 text-center">
+                        <h4> Bienvenido <strong> {{ Auth::user()->name }}</strong>
+                        </h4>
+                    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+
+
+    <x-footer/>
 @endsection
